0c900d0fd985568951a6b7e0603ea792f6cd360a
ulyssesronquillo <ulyssesronquillo@gmail.com>
2022-01-09 18:31:58 -0500

    updated dns-reset

--- dns-reset/resetdns.sh
+++ dns-reset/resetdns.sh
@@ -2,8 +2,8 @@
 
 elb_json="elb.json"
 web_json="web.json"
-wow_json="wowza.json"
-swi_json="switcher.json"
+wow_json="wow.json"
+swi_json="swi.json"
 
 aws route53 change-resource-record-sets --hosted-zone-id Z16AGEC3IMI3R8 --change-batch file://$web_json --profile tfc
 aws route53 change-resource-record-sets --hosted-zone-id Z16AGEC3IMI3R8 --change-batch file://$wow_json --profile tfc
